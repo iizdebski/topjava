49ee921daa4fa32c768add05c05c75bdb186ac18
ivan <iizdepskyy112@gmail.com>
2019-06-23 18:05:26 +0300

    1 7 Logging

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -6,11 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("users.jsp");
+      //  request.getRequestDispatcher("/users.jsp").forward(request, response);
+       response.sendRedirect("users.jsp");
     }
 }
