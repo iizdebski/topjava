220126aca8b2cc4db84a495d6dded067f8f77f42
ivan <iizdepskyy112@gmail.com>
2019-08-16 10:50:14 +0300

    10 01 HW9 binding ajax

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,59 @@
+const mealAjaxUrl = "ajax/profile/meals/";
+
+function updateFilteredTable() {
+    $.ajax({
+        type: "GET",
+        url: mealAjaxUrl + "filter",
+        data: $("#filter").serialize()
+    }).done(updateTableByData);
+}
+
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(mealAjaxUrl, updateTableByData);
+}
+
+$(function () {
+    makeEditable({
+        ajaxUrl: mealAjaxUrl,
+        datatableOpts: {
+            "columns": [
+                {
+                    "data": "dateTime",
+                    "render": function (date, type, row) {
+                        if (type === 'display') {
+                            return date.replace('T', ' ').substr(0, 16);
+                        }
+                        return date;
+                    }
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "render": renderEditBtn,
+                    "defaultContent": "",
+                    "orderable": false
+                },
+                {
+                    "render": renderDeleteBtn,
+                    "defaultContent": "",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-mealExcess", data.excess);
+            },
+        },
+        updateTable: updateFilteredTable
+    });
+});
